26a6b7b99614c23fdeefce27cbc342ddcd470dd9
Andrew Tolochka <me@andrewtolochka.com>
2022-05-22 13:31:18 +0200

    Reconfigured survey ratelimitting

--- api/views/survey.py
+++ api/views/survey.py
@@ -5,14 +5,17 @@ from ratelimit.decorators import ratelimit
 
 
 @api_view(['GET', ])
-@ratelimit(key='ip', rate='1/d', block=True)
+@ratelimit(key='ip', rate='1/d')
 def SurveyVoteView(request, survey_id, option_id):
-  try:
-    option = SurveyOption.objects.get(uuid=option_id, survey__pk=survey_id)
-  except SurveyOption.DoesNotExist:
-    raise Http404("Survey option does not exist")
+  was_limited = getattr(request, 'limited', False)
 
-  option.votes = option.votes + 1
-  option.save()
+  if not was_limited:
+    try:
+      option = SurveyOption.objects.get(uuid=option_id, survey__pk=survey_id)
+    except SurveyOption.DoesNotExist:
+      raise Http404("Survey option does not exist")
+
+    option.votes = option.votes + 1
+    option.save()
 
   return redirect("http://socialize.berlin/#thank-you-for-voting")
