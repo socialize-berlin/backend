28b8ecccf129b42085b6f9d3fd4c52f55c5da80b
Andrew Tolochka <me@andrewtolochka.com>
2022-05-22 12:40:16 +0200

    Removed unused property

--- api/models/user.py
+++ api/models/user.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CustomUserManager(UserManager):
     def get_queryset(self):
-        return super().get_queryset().filter(deleted=None)
+        return super().get_queryset()
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, blank=False)
@@ -14,7 +14,6 @@ class User(AbstractUser):
     lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     lng = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     avatar = models.ImageField(blank=True, default='', upload_to='avatars/')
-    deleted = models.DateTimeField(blank=True, null=True)
     is_confirmed = models.BooleanField(default=False)
     last_seen = models.DateTimeField(blank=True, null=True)
 
